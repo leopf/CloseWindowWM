83949c90844b52a1ac9f3c0868fea9405461b647
Leonhard Pfob <leo.pfob@outlook.de>
2019-09-20 13:54:46 +0200

    Projektdateien hinzufügen.

---
+++ CloseWindowWM/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static CloseWindowWM.HotKeyEventArgs;
+
+namespace CloseWindowWM
+{
+    class Program
+    {
+        private static string ErrorMessage = null;
+
+        static void Main(string[] args)
+        {
+            ErrorMessage = File.ReadAllText("errormsg.txt");
+
+            HotKeyManager.HotKeyPressed += (ss, ee) => CloseWindow();
+            HotKeyManager.RegisterHotKey(Keys.L, KeyModifiers.Alt);
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void CloseWindow()
+        {
+            Process process = WindowManager.GetForgroundProcess();
+            process.Kill();
+
+            string msg = ErrorMessage.Replace("[[processname]]", process.ProcessName);
+
+            MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ CloseWindowWM/WindowManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloseWindowWM
+{
+    public static class WindowManager
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out IntPtr ProcessId);
+
+        public static Process GetForgroundProcess()
+        {
+            IntPtr windowHwnd = GetForegroundWindow();
+            GetWindowThreadProcessId(windowHwnd, out IntPtr processHwnd);
+            return Process.GetProcessById(processHwnd.ToInt32());
+        }
+    }
+}
